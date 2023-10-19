889827154a3ab5b8a59ddb4ac82f8ad24e0dcff6
Nikolay Stoyanov <nikistoyanov2005@gmail.com>
2023-10-19 21:25:49 +0300

    .gitignore

---
+++ Shoe4U/Data/Models/User.cs
@@ -0,0 +1,22 @@
+﻿namespace Shoe4U.Data.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    
+    public string Name { get; set; }
+
+    public string PhoneNumber { get; set; }
+
+    public string Email { get; set; }
+
+    public string Password { get; set; }
+
+    public DateTime DateOfBirth { get; set; }
+
+    public string Address { get; set; }
+
+    public ICollection<Order> Orders { get; set; } = new HashSet<Order>();
+
+    public ICollection<Review> Reviews { get; set; } = new HashSet<Review>();
+}
