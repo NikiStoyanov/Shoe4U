df4f1079e1ad33f9043466a4477a883e0525e9f2
Stefani Kocheva <stefani291423@gmail.com>
2023-11-10 21:38:46 +0200

    Controller and Views Explained

--- src/Shoe4U/Controllers/HomeController.cs
+++ src/Shoe4U/Controllers/HomeController.cs
@@ -1,6 +1,12 @@
 ﻿namespace Shoe4U.Controllers;
 
-public class HomeController
-{
+using Microsoft.AspNetCore.Mvc;
 
+public class HomeController : Controller
+{
+    [HttpGet]
+    public IActionResult Index() => this.View();
+
+    [HttpGet]
+    public IActionResult Privacy() => this.View();
 }
