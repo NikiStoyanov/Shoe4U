5121b5d9d7dceb998f9d8b03d93b9c961dccccd7
Nikolay Stoyanov <nikistoyanov2005@gmail.com>
2023-11-18 21:51:06 +0200

    Added template

--- src/Shoe4U/Controllers/HomeController.cs
+++ src/Shoe4U/Controllers/HomeController.cs
@@ -8,5 +8,5 @@ public class HomeController : Controller
     public IActionResult Index() => this.View();
 
     [HttpGet]
-    public IActionResult Privacy() => this.View();
+    public IActionResult Contacts() => this.View();
 }
---
+++ src/Shoe4U/Controllers/ProductsController.cs
@@ -0,0 +1,15 @@
+﻿namespace Shoe4U.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+
+public class ProductsController : Controller
+{
+	[HttpGet]
+	public IActionResult Add() => this.View();
+
+	[HttpGet]
+	public IActionResult All() => this.View();
+
+	[HttpGet]
+	public IActionResult Details() => this.View();
+}
